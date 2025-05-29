16b7f5ae785cd9c98b6596edda148d7ab1860856
Mikołaj Storoniak <miko.storo@gmail.com>
2025-05-30 01:54:53 +0200

    added gitignore

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import cherrypy
-from cherrypy.lib import static
-import tinydb
+from cherrypy.lib import static, sessions
+from tinydb import TinyDB, Query
 from jinja2 import Template
 import resources.shards as Shards
 import os
+from hashlib import sha256
+import uuid
 
 localDir = os.path.dirname(__file__)
 absDir = os.path.join(os.getcwd(), localDir)
 
+user_db = TinyDB(os.path.abspath("./databases/users.json"))
+
 conf={
+        
        '/style.css':
                     { 'tools.staticfile.on':True,
                       'tools.staticfile.filename': os.path.abspath("./resources/style.css"),
@@ -26,20 +31,64 @@ conf={
                       'tools.staticfile.filename': os.path.abspath("./resources/panels.js"),
                     }
     }
+conf = { '/' : { 'tools.sessions.on': True },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.abspath("./resources")
+        }
+        }
 
 class StoroDrive(object):
 
+
     @cherrypy.expose
     def index(self):
-        return open('resources/index.html', encoding='utf-8')
+        page_text = open('resources/index.html', encoding='utf-8').read()
+        template = Template(page_text)
+        data = {}
+        if 'user' in cherrypy.session:
+            data['uname'] = cherrypy.session['user']['username']
+            data['catalogue'] = cherrypy.session['user']['catalogue']
+        return template.render(data)
 
     @cherrypy.expose
     def storage(self, catalogue="common"):
-        content = Shards.get_documents(catalogue)
+        path = os.path.abspath("./volume/" + catalogue)
+        if not os.path.exists(path):
+            os.makedirs(path, mode=0o777, exist_ok=False)
+        content = Shards.get_documents_table(catalogue)
         data = { "id" : catalogue, "content" : content}
         page_text = open('resources/storage.html', encoding='utf-8').read()
         template = Template(page_text)
         return template.render(data)
+
+    @cherrypy.expose
+    def login(self, username, password):
+        #pswd_hash = sha256(password.encode)
+        pswd_hash = password
+        user_record = user_db.search(Query().username == username)[0]
+        print(user_record)
+        if user_record["pswd_hash"] == pswd_hash:
+            cherrypy.session['user'] = user_record
+        return self.index()
+
+    @cherrypy.expose
+    def register(self):
+        return open('resources/register.html', encoding='utf-8')
+
+    @cherrypy.expose
+    def create_user(self, username, password, confirm):
+        if(password == confirm):
+            #pswd_hash = sha256(password.encode())
+            pswd_hash = password
+            user_db.insert({
+                'username': username, 
+                'pswd_hash': pswd_hash, 
+                'catalogue': str(uuid.uuid1()), 
+                'user_id': str(uuid.uuid1())
+            })
+        return self.index()
+    
 
     @cherrypy.expose
     def download(self, filename, catalogue="common"):
@@ -69,5 +118,11 @@ class StoroDrive(object):
             self._upload_file(file,catalogue)
         return self.storage(catalogue)
 
+    @cherrypy.expose
+    def delete(self, filename, catalogue):
+        delete_path = os.path.join(absDir,"volume",catalogue, filename)
+        os.remove(delete_path)
+        return self.storage(catalogue)
+
 if __name__ == '__main__':
-    cherrypy.quickstart(StoroDrive(), config=conf)
+    cherrypy.quickstart(StoroDrive(), "/" ,config=conf)
--- resources/shards.py
+++ resources/shards.py
@@ -1,7 +1,7 @@
 import os
 
 
-def get_document(filename,catalogue="common"):
+def get_document(filename:str,catalogue="common"):
     element = '<div class="document">' 
     element += '<a href="download?catalogue=' + catalogue + '&filename=' + filename +'">'
     element += filename
@@ -9,19 +9,39 @@ def get_document(filename,catalogue="common"):
     element += '</div>'
     return element
 
-def get_document_table_row(filename, catalogue="common"):
-    pass
+def get_document_table_row(filename:str, catalogue="common"):
+    ## nazwa, pobierz, usuń
+    row = """<tr> 
+                <td>{0}</td> 
+                <td>{1}</td>
+                <td>{2}</td>
+            </tr>""".format(filename, 
+                            get_download_btn(filename, catalogue),
+                            get_delete_button(filename, catalogue))
+    return row
 
-def get_document_download_link(filename, catalogue="common"):
-    pass
+def get_delete_button(filename:str, catalogue="common"):
+    return '<button> <a href="delete?catalogue={0}&filename={1}" class="no_decor"> 🪓 </a> </button>'.format(catalogue,filename) 
 
-def get_documents_table(catalogue="common"):
-    pass
 
-def get_documents(catalogue):
+def get_download_btn(filename:str, catalogue="common"):
+    return '<button> <a href="download?catalogue={0}&filename={1}" class="no_decor"> 📥 </a> </button>'.format(catalogue,filename)
+
+def get_documents_table(catalogue:str="common"):
+    table = "<table class='centered_table'> <tr>"
+    table += "<th> Nazwa </th>"
+    table += "<th> Pobierz </th>"
+    table += "<th> Usuń </th>"
+    table += "</tr>"
+    for d in get_documents(catalogue):
+        table += get_document_table_row(d, catalogue)
+    table += "</table>"
+    return table
+
+def get_documents(catalogue:str):
     path = os.path.join("volume", catalogue)
-    elements = ''
+    elements = []
     for filename in os.listdir(path):
-        elements += get_document(filename,catalogue)
+        elements.append(filename)
     return elements
 
